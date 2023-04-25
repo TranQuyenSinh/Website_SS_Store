f1332e4e6599ecf08e1e8dc95d94d43ec3c7d1dc
Quyen Sinh <vn.quyensinh@gmail.com>
2023-04-25 07:14:17 +0700

    profile

---
+++ Client/PHP/profile.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="../css/profile.css" />
+<div class="avatar">
+				<img
+					src="https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg"
+					alt="avatar"
+				/>
+			</div>
+			<div class="info">
+				<h1>Hồ sơ của tôi :</h1>
+				<form>
+					<label for="username">Tên đăng nhập:</label><br />
+					<input type="text" id="username" name="username" /><br />
+					<label for="phone">Số điện thoại:</label><br />
+					<input type="text" id="phone" name="phone" /><br />
+					<label>Giới tính:</label>
+					<input type="radio" id="male" name="gender" value="nam" />
+					<label for="male">Nam</label><br />
+					<input type="radio" id="female" name="gender" value="nữ" />
+					<label for="female">Nữ</label><br />
+					<button type="submit">Lưu</button>
+				</form>
+			</div>
+<?php
+    
+?>
